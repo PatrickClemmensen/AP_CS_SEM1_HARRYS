2185ff86f556c055e13f62b9becb9166f7940fc7
nickichandk <nicklas.marc@gmail.com>
2026-03-19 11:43:51 +0100

    Customer i role-klassen er fikset med attributer, conctructor og getters

--- src/model/roles/Customer.java
+++ src/model/roles/Customer.java
@@ -1,4 +1,22 @@
 package model.roles;
 
-public class Customer {
+public class Customer  {
+
+    private String name;
+    private String phoneNumber;
+
+
+    public Customer(String name, String phoneNumber){
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
 }
