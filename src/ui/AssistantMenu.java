b08d2d7727c65257597b32a46c973eae5ac2f798
ispa070 <59nyd8m94g@privaterelay.appleid.com>
2026-03-19 16:23:17 +0100

    Implemented inputvalidation for AssistantMenu createBooking() but only for "date".

--- src/ui/AssistantMenu.java
+++ src/ui/AssistantMenu.java
@@ -4,9 +4,13 @@ import model.appointments.Appointment;
 import model.appointments.TimeSlot;
 import model.roles.Customer;
 import service.FileStorage;
+import util.exceptions.InvalidDateException;
+import util.exceptions.InvalidInputException;
+import util.inputvalidation.InputValidator;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 
 public class AssistantMenu {
@@ -40,22 +44,30 @@ public class AssistantMenu {
     }
 
     private void printMenu() {
-            System.out.println("\nWelcome to the Assistant Menu");
-            System.out.println("------------------------------------------");
-            System.out.println("1. Create a new booking");
-            System.out.println("2. Delete an existing booking");
-            System.out.println("3. View all appointments");
+        System.out.println("\nWelcome to the Assistant Menu");
+        System.out.println("------------------------------------------");
+        System.out.println("1. Create a new booking");
+        System.out.println("2. Delete an existing booking");
+        System.out.println("3. View all appointments");
     }
 
     private void createBooking() {
-        System.out.print("Enter customer name: ");
+        System.out.print("\nEnter customer name: ");
         String name = scanner.nextLine();
 
         System.out.print("Enter phone number of the customer: ");
         String phoneNumber = scanner.nextLine();
 
-        System.out.print("Select a date for the booking (YYYY-MM-DD): ");
-        LocalDate date = LocalDate.parse(scanner.nextLine());
+        LocalDate date = null;
+        while(true) {
+            System.out.print("Select a date for the booking (YYYY-MM-DD): ");
+            try {
+                date = InputValidator.validateDate(scanner.nextLine());
+                break;
+            } catch (InvalidDateException | InvalidInputException | DateTimeParseException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
 
         System.out.print("Select a start time for the booking (HH:MM): ");
         LocalTime startTime = LocalTime.parse(scanner.nextLine());
@@ -64,7 +76,15 @@ public class AssistantMenu {
         LocalTime endTime = LocalTime.parse(scanner.nextLine());
 
         Customer customer = new Customer(name, phoneNumber);
-        FileStorage.addAppointment(new Appointment(date,new TimeSlot(startTime, endTime), customer));
+        FileStorage.addAppointment(new Appointment(date, new TimeSlot(startTime, endTime), customer));
+
+        System.out.print("Booking added!\nType 'yes' to add another booking, type 'no' to return to the Assistant Menu.");
+        String answer = scanner.nextLine();
+        if (answer.equals("yes")) {
+            createBooking();
+        } else if (answer.equals("no")) {
+            printMenu();
+        }
     }
 
     private void deleteBooking() {
