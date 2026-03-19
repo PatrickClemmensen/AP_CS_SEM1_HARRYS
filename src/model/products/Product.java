c010dcf60ecdcbd7e200a0f4f52a09c07687ebf1
PatrickClemmensen <patrickclemmensen@gmail.com>
2026-03-19 12:02:28 +0100

    Products & category enum created.

--- src/model/products/Product.java
+++ src/model/products/Product.java
@@ -1,4 +1,33 @@
 package model.products;
 
-public class Product {
+public enum Product {
+
+    // Services
+    MENS_HAIRCUT(250.0, Category.SERVICE),
+    WOMENS_HAIRCUT(350.0, Category.SERVICE),
+
+    // Retail
+    SHAMPOO(45.0, Category.RETAIL),
+    HAIRBRUSH(89.0, Category.RETAIL);
+
+    private final double price;
+    private final Category category;
+
+    Product(double price, Category category) {
+        this.price = price;
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public enum Category {
+        SERVICE,
+        RETAIL
+    }
 }
