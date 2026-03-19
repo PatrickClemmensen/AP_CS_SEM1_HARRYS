ef3025c69c48a7a900dc9331b86585cac2a3bdc4
nickichandk <nicklas.marc@gmail.com>
2026-03-19 16:24:05 +0100

    Accoutant and owner skeleton

--- src/ui/OwnerMenu.java
+++ src/ui/OwnerMenu.java
@@ -1,4 +1,44 @@
 package ui;
 
+import java.util.Scanner;
+
 public class OwnerMenu {
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public OwnerMenu() {
+
+    }
+
+    public void show() {
+        boolean running = true;
+
+        while (running) {
+
+            printMenu();
+            int choice = Integer.parseInt(scanner.nextLine());
+
+            switch (choice) {
+                case 1:
+                    createBooking();
+                    break;
+                case 2:
+                    deleteBooking();
+                    break;
+                case 3:
+                    viewAppointments();
+                    break;
+
+            }
+        }
+    }
+
+    private void printMenu() {
+        System.out.println("\nWelcome to the Owners Menu");
+        System.out.println("------------------------------------------");
+        System.out.println("1. Create a new booking");
+        System.out.println("2. Delete an existing booking");
+        System.out.println("3. View all appointments");
+    }
+
 }
