a69aac8896e50b282f0a91560787778e04a4c8e7
nickichandk <nicklas.marc@gmail.com>
2026-03-24 12:58:59 +0100

    Exceptions and InputValidator has java doc

--- src/util/exceptions/InvalidInputException.java
+++ src/util/exceptions/InvalidInputException.java
@@ -1,6 +1,14 @@
 package util.exceptions;
 
+/**
+ * Exception thrown when user input fails validation.
+ */
 public class InvalidInputException extends RuntimeException {
+    /**
+     * Creates a new InvalidInputException with the given message.
+     *
+     * @param message the detail message explaining why the input is invalid
+     */
     public InvalidInputException(String message) {
         super(message);
     }
